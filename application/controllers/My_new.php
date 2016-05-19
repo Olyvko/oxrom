01f458e3e37838cc237f2541755b83d3b5da7e22
Roman Olyvko <r.olyvko@zinitsolutions.com>
2016-05-19 22:51:58 +0300

    h_0619

--- application/controllers/My_new.php
+++ application/controllers/My_new.php
@@ -11,11 +11,19 @@ class My_new extends CI_Controller {
         $this->load->model('my_new_model');
         $aRes = $this->my_new_model->getInfo();
 
+        //redirect('../');
+       // redirect($this->config->item('base_url').'/my_new');
         var_dump($aRes);
+        var_dump($this->config->item('base_url'));
         $this->load->helper('my_new_helper');
         var_dump(my_test());
         
         $this->load->view('my_new');
+    }
+
+    public function index()
+    {
+        $this->load->view('welcome_message');
     }
 
 }
